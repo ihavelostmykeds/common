80f53f14bc9d947d6d33de483293f979389f5a4f
Sasha Zasanskyi <glorylastsforever@ukr.net>
2019-09-23 21:04:52 +0300

    commititng changes to git_homework branch

--- git/homework.py
+++ git/homework.py
@@ -10,12 +10,12 @@ class OurAwesomeException(Exception):
     pass
 
 
-def is_two_object_has_same_value(first: Any, second: Any) -> bool:
+def is_two_object_has_same_value(first, second):
     """
     If @first and @second has same value should return True
     In another case should return False
     """
-    pass
+    return first == second
 
 
 def is_two_objects_has_same_type(first: Any, second: Any) -> bool:
@@ -23,48 +23,49 @@ def is_two_objects_has_same_type(first: Any, second: Any) -> bool:
     If @first and @second has same type should return True
     In another case should return False
     """
-    pass
+    return type(first) == type(second)
 
 
-def is_two_objects_is_the_same_objects(first: Any, second: Any) -> bool:
+def is_two_objects_is_the_same_objects(first, second) -> bool:
     """
-    If @first and @second has same type should return True
+    If @first and @second is the same object should return True
     In another case should return False
     """
-    pass
+    return first is second
 
 
 def multiple_ints(first_value: int, second_value: int) -> int:
     """
     Should calculate product of all args.
     if first_value or second_value is not int should raise ValueError
-
     Raises:
         ValueError
-
     Params:
         first_value: value for multiply
         second_value
     Returns:
         Product of elements
     """
-    pass
+    try:
+        int(first_value)
+        int(second_value)
+        return first_value*second_value
+    except:
+        raise ValueError
+
+
 
 
 def multiple_ints_with_conversion(first_value: Any, second_value: Any) -> int:
     """
     If possible to convert arguments to int value - convert and multiply them.
     If it is impossible raise ValueError
-
     Args:
         first_value: number for multiply
         second_value: number for multiply
-
     Raises:
         ValueError
-
     Returns: multiple of two numbers.
-
     Examples:
         multiple_ints_with_conversion(6, 6)
         >>> 36
@@ -78,33 +79,46 @@ def multiple_ints_with_conversion(first_value: Any, second_value: Any) -> int:
             print("Not valid input data")
         >>> "Not valid input data"
     """
-    pass
+    try:
+        int(first_value)
+        int(second_value)
+        return int(first_value)*int(second_value)
+    except:
+        raise ValueError('Not valid input data')
+    
 
 
 def is_word_in_text(word: str, text: str) -> bool:
     """
     If text contain word return True
     In another case return False.
-
     Args:
         word: Searchable substring
         text: Text for searching
-
     Examples:
         is_word_in_text("Hello", "Hello word")
         >>> True
         is_word_in_text("Glad", "Nice to meet you ")
         >>> False
-
     """
-    pass
+    if word in text:
+        return True
+    else:
+        return False
 
 
 def some_loop_exercise() -> list:
     """
     Use loop to create list that contain int values from 0 to 12 except 6 and 7
     """
-    pass
+    my_perfect_list = []
+    for i in range(0, 13):
+        if i == 6 or i == 7:
+            continue
+        else:
+            my_perfect_list.append(i)
+    return my_perfect_list
+
 
 
 def remove_from_list_all_negative_numbers(data: List[int]) -> list:
@@ -116,7 +130,12 @@ def remove_from_list_all_negative_numbers(data: List[int]) -> list:
         remove_from_list_all_negative_numbers([1, 5, -7, 8, -1])
         >>> [1, 5, 8]
     """
-    pass
+    new_list = data[:]
+    for i in data:
+        if i < 0:
+            new_list.remove(i)
+    return new_list
+
 
 
 def alphabet() -> dict:
@@ -127,7 +146,13 @@ def alphabet() -> dict:
         alphabet()
         >>> {"a": 1, "b": 2 ...}
     """
-    pass
+        
+    alphabet_array = {}
+    step = 1
+    for i in range(ord('a'), ord('z') + 1):
+        alphabet_array[step] = chr(i)
+        step += 1
+    return alphabet_array
 
 
 def simple_sort(data: List[int]) -> List[list]:
@@ -137,6 +162,16 @@ def simple_sort(data: List[int]) -> List[list]:
         simple_sort([2, 9, 6, 7, 3, 2, 1])
         >>> [1, 2, 2, 3, 6, 7, 9]
     Returns:
-
     """
-    pass
+    new_list = []
+    while data:
+    	minimum = data[0]  # arbitrary number in list 
+    	for i in data: 
+            if i < minimum:
+                minimum = i
+    	new_list.append(minimum)
+    	data.remove(minimum)    
+
+    return new_list
+
+  
