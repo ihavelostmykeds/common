da369942eed2ce92f0154d17eb237e7e1a84a78a
Sasha Zasanskyi <glorylastsforever@ukr.net>
2019-10-29 13:23:47 +0200

    Flask hw done

--- flask_lesson_intro/main.py
+++ flask_lesson_intro/main.py
@@ -6,7 +6,22 @@ app = Flask(__name__)
 
 @app.route('/')
 def get_home_page():
-    return render_template("home.html", data=get_data())
+    return render_template("home.html")
+
+
+@app.route('/base')
+def get_base_page():
+    return render_template("base.html")
+
+
+@app.route('/vegetables')
+def get_vegetables_page():
+    return render_template("vegetables.html", vegetables=['beans', 'carrot', 'beetroot', 'cucumber'])
+
+
+@app.route('/fruits')
+def get_fruit_page():
+    return render_template("fruits.html", fruits=['melon', 'apple', 'strawberry', 'grape'])
 
 
 if __name__ == "__main__":
